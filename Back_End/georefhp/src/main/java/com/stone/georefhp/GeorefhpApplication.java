b2e7a005659633f873d7c07baaa9bd21229c8401
Youness LAAOUANE <61345973+ynsl31@users.noreply.github.com>
2020-12-17 17:27:07 +0100

    Back End init

---
+++ Back_End/georefhp/src/main/java/com/stone/georefhp/GeorefhpApplication.java
@@ -0,0 +1,13 @@
+package com.stone.georefhp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeorefhpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeorefhpApplication.class, args);
+	}
+
+}
---
+++ Back_End/georefhp/src/test/java/com/stone/georefhp/GeorefhpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stone.georefhp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeorefhpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
