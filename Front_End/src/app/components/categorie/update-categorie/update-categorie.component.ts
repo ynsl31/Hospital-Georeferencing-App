72f865e4cb9ea158bb29d078285652f07a49d7c0
Youness LAAOUANE <61345973+ynsl31@users.noreply.github.com>
2021-01-17 19:18:48 +0100

    Fix Categorie (update+Delete)

--- Front_End/src/app/components/categorie/update-categorie/update-categorie.component.ts
+++ Front_End/src/app/components/categorie/update-categorie/update-categorie.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Categorie } from 'src/app/modals/categorie';
+import { CategorieService } from '../categorie.service';
 
+declare var $: any;
 @Component({
   selector: 'app-update-categorie',
   templateUrl: './update-categorie.component.html',
@@ -7,9 +11,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateCategorieComponent implements OnInit {
 
-  constructor() { }
+ @Input()  categorie : Categorie;
+
+  @Output() isClosed = new EventEmitter<boolean>();
+
+  categorieForm: FormGroup
+
+  constructor(
+    private form: FormBuilder,
+    private categorieService: CategorieService
+  ) {
+
+
+    this.categorieForm = form.group({
+
+      nom: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+    
+
+    });
+
+  }
 
   ngOnInit() {
+  
+  }
+
+
+
+  updateCategorie() {
+
+    let categorie = new Categorie();
+
+    categorie = this.categorieForm.value;
+    categorie.id = this.categorie.id
+
+
+    this.categorieService.createCategorie(categorie).subscribe(
+      data => {
+
+        this.categorieForm.reset();
+
+        this.isClosed.emit(true);
+
+        $('.modal').modal('hide');
+
+      },
+      error => {
+        console.log("error");
+      },
+      () => { console.log('loading Done') }
+    );
+
+
   }
 
 }
